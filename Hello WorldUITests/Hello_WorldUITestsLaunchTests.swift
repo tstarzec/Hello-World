1d69dd50614d5dd6484ae023ad3eaf348691fa1d
tstarzec <tstar391@d127.org>
2022-09-29 08:51:55 -0500

    Initial Commit

---
+++ Hello WorldUITests/Hello_WorldUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hello_WorldUITestsLaunchTests.swift
+//  Hello WorldUITests
+//
+//  Created by Tristan L Starzec (Student) on 9/29/22.
+//
+
+import XCTest
+
+class Hello_WorldUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
